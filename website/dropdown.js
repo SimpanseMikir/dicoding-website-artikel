b5fd516643c55a73dfa632dd32561ff97498c0c4
SimpanseMikir <vian536@gmail.com>
2024-12-29 01:00:10 +0700

    memperbaiki coding

--- website/dropdown.js
+++ website/dropdown.js
@@ -1,36 +1,32 @@
-const dropdown = document.querySelector('.dropdown');
+document.addEventListener('DOMContentLoaded', () => {
+    const dropdowns = document.querySelectorAll('.dropdown');
 
-dropdowns.foreach(dropdown => {
-    const select = dropdown.querySelector('.select');
-    const caret = dropdown.querySelector('.caret');
-    const menu = dropdown.querySelector('.menu');   
-    const options = dropdown.querySelectorAll('.menu li');
-    const selected = dropdown.querySelector(.selected);
+    dropdowns.forEach(dropdown => {
+        const select = dropdown.querySelector('.select');
+        const caret = dropdown.querySelector('.caret');
+        const menu = dropdown.querySelector('.menu');   
+        const options = dropdown.querySelectorAll('.menu li');
+        const selected = dropdown.querySelector('.selected');
+
+        select.addEventListener('click', () => {
+            select.classList.toggle('open');
+            caret.classList.toggle('open');
+            menu.classList.toggle('open');
+        });
+
+        options.forEach(option => {
+            option.addEventListener('click', () => {
+                selected.innerText = option.innerText;
+                select.classList.remove('open');
+                caret.classList.remove('open');
+                menu.classList.remove('open');
+
+                options.forEach(opt => {
+                    opt.classList.remove('active');
+                });
+
+                option.classList.add('active');
+            });
+        });
+    });
 });
-    select.addEventListener('click', () => {
-        select.classList.toggle('open');
-        caret.classList.toggle('open');
-        menu.classList.toggle('open');
-    });
-    options.forEach(option => {
-        option.addEventListener('click', () => {
-            selected.innerText = option.innerText;
-            select.classList.remove('open');
-            caret.classList.remove('open');
-            menu.classList.remove('open');
-        });
-    });
-
-    options.forEach(option => {
-        option.addEventListener('click', () => {
-            selected.innerText = option.innerText;
-            select.classList.remove('open');
-            caret.classList.remove('open');
-            menu.classList.remove('open');
-       option.forEach(option => {
-        option.classList.remove('active');
-        });
-
-        option.classlist.add('active');
-        });
-    });
